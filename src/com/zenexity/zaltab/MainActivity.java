785a98baee02c3c281236905bd9bc5181bd48f27
Adrien Maillol <ama@zenexity.com>
2013-10-02 09:48:15 +0200

    Touch service and stuff

--- src/com/zenexity/zaltab/MainActivity.java
+++ src/com/zenexity/zaltab/MainActivity.java
@@ -4,7 +4,6 @@ import android.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
