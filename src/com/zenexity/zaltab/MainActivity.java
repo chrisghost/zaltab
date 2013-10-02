98c2a1ee5fb9a747f9dbf6a26616388c75c9f3fb
Adrien Maillol <ama@zenexity.com>
2013-10-02 09:27:23 +0200

    Add activity an service

---
+++ src/com/zenexity/zaltab/MainActivity.java
@@ -0,0 +1,38 @@
+package com.zenexity.zaltab;
+
+import android.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	 Intent globalService;
+	 @Override
+	 protected void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  setContentView(R.layout.activity_list_item);
+	  globalService = new Intent(this, TouchService.class);
+	 }
+
+
+	 public void buttonClicked(View v){
+	  
+	  if(v.getTag() == null){
+	   startService(globalService);
+	   v.setTag("on");
+	   Toast.makeText(this, "Start Service", Toast.LENGTH_SHORT).show();
+	  }
+	  else{
+	   stopService(globalService);
+	   v.setTag(null);
+	   Toast.makeText(this, "Stop Service", Toast.LENGTH_SHORT).show();
+	  }
+	  
+	 }
+
+
+}
