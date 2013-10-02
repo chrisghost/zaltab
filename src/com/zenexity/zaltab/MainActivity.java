d988ee46020576726bf04f280127ffc6bc842978
Maxime Calmels <mca@zenexity.com>
2013-10-02 11:37:08 +0200

    Get recent and running app

--- src/com/zenexity/zaltab/MainActivity.java
+++ src/com/zenexity/zaltab/MainActivity.java
@@ -1,24 +1,49 @@
 package com.zenexity.zaltab;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RecentTaskInfo;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.app.ActivityManager.RunningTaskInfo;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	 Intent globalService;
 	 @Override
-	 protected void onCreate(Bundle savedInstanceState) {
+	 public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
 	  setContentView(R.layout.activity_main);
 	  globalService = new Intent(this, TouchService.class);
+	  
 	 }
 
 
 	 public void buttonClicked(View v){
-	  
+	  final TextView vTextView2 = (TextView)findViewById(R.id.textView2);
+	  final TextView vTextView1 = (TextView)findViewById(R.id.textView1);
+	  final ImageView imgV = (ImageView)findViewById(R.id.imageView1);
+	  ActivityManager manager = 
+			    (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+			List<RunningTaskInfo> rtinfo = manager.getRunningTasks(1);
+			List<RecentTaskInfo> rtis = manager.getRecentTasks(5,ActivityManager.RECENT_WITH_EXCLUDED);
+			List<RunningTaskInfo> taskInfos = null;
+			//GET recent Application
+			//for (RecentTaskInfo rti : rtis)
+			//{
+				//vTextView2.setText(vTextView2.getText() + "\n" + rti.id );
+	 		//}
+			for (RunningTaskInfo rt : rtinfo) {
+				imgV.setImageBitmap(rt.thumbnail);
+			}
+			//}
 	  if(v.getTag() == null){
 	   startService(globalService);
 	   v.setTag("on");
