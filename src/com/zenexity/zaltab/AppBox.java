d4ad28b3a2cad4ffa38c4301ee05fa6adcdd574d
Adrien Maillol <ama@zenexity.com>
2013-10-02 17:23:56 +0200

    Forgot file

---
+++ src/com/zenexity/zaltab/AppBox.java
@@ -0,0 +1,11 @@
+package com.zenexity.zaltab;
+
+import android.app.ActivityManager.RecentTaskInfo;
+import android.content.pm.ApplicationInfo;
+import android.widget.ImageView;
+
+public class AppBox {
+	public ApplicationInfo appInfo = null;
+	public RecentTaskInfo taskInfo = null;
+	public ImageView imageView = null;
+}
